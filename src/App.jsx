720f290c0ba8441cb385f4a32997924bd1c92006
Renan Santos <renansantos7@proton.me>
2023-12-28 16:56:29 -0300

    iniciando a interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import dadosApps from '.data/apps.json'
 import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 function App() {
+  const [apps, setApps] = useState(dadosApps)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/'element={<PaginaPadrao />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
